fa1351d29f18f6588b7c4d5d27caa1542779f7ba
seven272 <i727i@mail.ru>
2026-01-23 19:18:46 +0300

    update

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -5,7 +5,7 @@ console.log(mode)
 const BASE_URL =
  import.meta.env.MODE === 'development'
     ? 'http://localhost:5030/api'
-    : '/api'
+    : 'app5/api'
 //  import.meta.env.MODE === 'development'
 //     ? 'http://prank-sound.ru/project1/api'
 //     : '/api'
