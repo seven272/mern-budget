f9b230371b8a9c5974bb997bfa3c80a3bf9dae24
seven272 <i727i@mail.ru>
2026-01-25 18:06:46 +0300

    без nginx-rp

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,11 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/myapp',
+  base: '/',
   server: {
     host: true,
     port: 5173, // This is the port which we will use in docker
-    allowedHosts: ['http://185.251.89.77', 'http://185.251.89.77:8030', 'prank-sound.ru', 'www.prank-sound.ru'],
+    allowedHosts: [
+      'http://185.251.89.77',
+      'http://185.251.89.77:8030',
+      'prank-sound.ru',
+      'www.prank-sound.ru',
+    ],
   },
   preview: {
     port: 4444,
